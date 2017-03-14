5bc4643d1361ad30e97f7b371e48cfc3ba92d278
Jason Li <li07.jason@gmail.com>
2017-03-14 03:07:26 -0400

    add labels, fix warnings

--- APCSP_CreatePT/MainWin.h
+++ APCSP_CreatePT/MainWin.h
@@ -6,7 +6,15 @@ typedef struct {
 	INT count;
 	HWND paintWin;
 	HWND massSlider;
+	HWND massSliderNameLabel;
+	HWND massSliderMinLabel;
+	HWND massSliderMaxLabel;
+	HWND massSliderValueLabel;
 	HWND lengthSlider;
+	HWND lengthSliderNameLabel;
+	HWND lengthSliderMinLabel;
+	HWND lengthSliderMaxLabel;
+	HWND lengthSliderValueLabel;
 } MainWinData;
 
 #define MAINWINCLASS L"MainWindowClassName"
